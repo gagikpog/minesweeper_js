76e483051640932f125dfe8010500805a44dd45f
Gagik <gagikpog@gmail.com>
2023-03-18 12:24:17 +0500

    view render

--- components/view.tsx
+++ components/view.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties } from "react";
+import { CSSProperties, useMemo } from "react";
 import { useSelector } from "react-redux";
 import { mapGetter } from "../game/funcs/getters";
 import { EventType, IMapItem } from "../game/types";
@@ -17,27 +17,24 @@ export default function View(props: IViewProps): JSX.Element {
 
     const gameMap = useSelector((state: RootState) => state.gameMap);
 
+    const items = useMemo(() => {
+        return Array(props.height * props.width).fill(null);
+    }, [props.width, props.height])
+
     return (
-        <div style={ props.style }>
+        <div style={ props.style } className="tw-flex tw-flex-wrap">
             {
-                // TODO: Придумать что то получке
-                Array(props.height).fill(null).map((_, rowIndex) => {
-                    return (
-                        <div key={rowIndex} className='tw-flex'>
-                            {
-                                Array(props.width).fill(null).map((_, cellIndex) => {
-                                    const item = mapGetter(gameMap, rowIndex, cellIndex);
-                                    return <Item
-                                        key={cellIndex}
-                                        val={ item?.val }
-                                        state={ item?.state }
-                                        row={rowIndex}
-                                        cell={cellIndex}
-                                        onClick={props.itemClick} />
-                                })
-                            }
-                        </div>
-                    );
+                items.map((_, index) => {
+                    const rowIndex = Math.floor(index / props.width);
+                    const cellIndex = index % props.width;
+                    const item = mapGetter(gameMap, rowIndex, cellIndex);
+                    return <Item
+                        key={`${rowIndex}-${cellIndex}`}
+                        val={ item?.val }
+                        state={ item?.state }
+                        row={rowIndex}
+                        cell={cellIndex}
+                        onClick={props.itemClick} />
                 })
             }
         </div>
