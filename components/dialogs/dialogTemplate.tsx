5e9ccbcc98a09228b3a8a4d483d4eb8ddf5078cd
Gagik <gagikpog@gmail.com>
2023-03-19 19:29:02 +0500

    dialog template

---
+++ components/dialogs/dialogTemplate.tsx
@@ -0,0 +1,29 @@
+import { ReactElement } from "react";
+import { IDialogProps } from "../../dialog/dialogProvider";
+import styles from '../../styles/DialogTemplate.module.css';
+
+interface IProps extends IDialogProps {
+    closeButtonVisible?: boolean;
+    children: ReactElement;
+}
+
+export default function DialogTemplate(props: IProps) {
+
+    const { closeButtonVisible = true, title, children, close } = props;
+
+    return (
+        <div className={styles.content}>
+            { title ? <div className={`${styles.header} tw-p-8`}> { title } </div> : null }
+            {
+                closeButtonVisible ? <div className={styles.closeWrapper} onClick={close}>
+                    <div className={styles.closeBack}>
+                        <div className={styles.close}>
+                            &times;
+                        </div>
+                    </div>
+                </div> : null
+            }
+            <div className={styles.body}> { children } </div>
+        </div>
+    );
+}
