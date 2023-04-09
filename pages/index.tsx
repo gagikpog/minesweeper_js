360c83b96fcd52e40c5af14d1fc9275eedb0d9ee
Gagik <gagikpog@gmail.com>
2023-04-09 21:11:00 +0500

    fix manifest

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import { Inter } from '@next/font/google'
 import Home from '../components/home';
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux';
 import { store } from '../store/main';
 import Previewer from '../components/previewer';
 import { DialogProvider } from '../dialog/dialogProvider';
 import { applicationInit } from '../game/funcs/applicationInit';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,12 +13,21 @@ applicationInit();
 
 export default function Index() {
     return (
-        <Provider store={store}>
-            <DialogProvider>
-                <Previewer>
-                    <Home />
-                </Previewer>
-            </DialogProvider>
-        </Provider>
+        <>
+            <Head>
+                <title>Minesweeper</title>
+                <meta name='viewport' content='user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=320, height=device-height' />
+                <link rel='manifest' href='/.webmanifest' />
+                <meta http-equiv='X-UA-Compatible' content='ie=edge' />
+                <link rel='icon' type='image/x-icon' href='/favicon.ico'></link>
+            </Head>
+            <Provider store={store}>
+                <DialogProvider>
+                    <Previewer>
+                        <Home />
+                    </Previewer>
+                </DialogProvider>
+            </Provider>
+        </>
     )
 }
